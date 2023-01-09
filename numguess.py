7e5198ff03dde7d7a234ad0eedc1b450c43cfb15
minje <jacksinger@naver.com>
2023-01-09 16:30:17 +0900

    feat: Create number guess
    generate the randum number by using randint
    input the user name and the user's guess number

---
+++ numguess.py
@@ -0,0 +1,14 @@
+
+# Generate the random number
+from random import randint
+answer = randint(1,100)
+
+# User interaction
+user_name = input("사용자 이름: ")
+print('안녕, {}! 숫자를 맞춰봐'.format(user_name))
+guess_answer = input("추측한 정답: ")
+
+if answer == guess_answer:
+    print("{}가 맞습니다.".format(guess_answer))
+else:
+    print("틀렸습니다.")
